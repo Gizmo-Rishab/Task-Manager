794eb4ddced1ccb3bf39e90a19c8386d7439ce7a
Rishab Arora <rishabarora2008@gmail.com>
2019-09-07 22:01:32 +0530

    Added more test cases, mocked sgMail

--- tests/user.test.js
+++ tests/user.test.js
@@ -11,7 +11,7 @@ const user1 = {
     email: 'waffles@choco.io',
     password: 'ChocoWaffles100@',
     tokens: [{
-        token: jwt.sign({ _id1 }, process.env.JWT_SECRET)
+        token: jwt.sign({ _id: _id1 }, process.env.JWT_SECRET)
     }]
 }
 
@@ -21,18 +21,35 @@ beforeEach(async () => {
 })
 
 test('Should sign up a new user', async () => {
-    await request(app).post('/users').send({
+    const response = await request(app).post('/users').send({
         name: 'Rishab',
         email: 'rishabarora2008@googlemail.com',
         password: 'MyCode765$'
     }).expect(201)
+
+    // Asserts that something is changed correctly in the DB
+    const user = await User.findById(response.body.user._id)
+    expect(user).not.toBeNull()
+
+    // Assertions about response
+    expect(response.body).toMatchObject({
+        user: {
+            name: 'Rishab',
+            email: 'rishabarora2008@googlemail.com'
+        },
+        token: user.tokens[0].token
+    })
+    expect(user.password).not.toBe('MyCode765$')
 })
 
 test('Should login a existing user', async () => {
-    await request(app).post('/users/login').send({
+    const response = await request(app).post('/users/login').send({
         email: user1.email,
         password: user1.password
     }).expect(200)
+
+    const user = await User.findById(_id1)
+    expect(response.body.token).toBe(user.tokens[1].token)
 })
 
 test('Should not login a non-existing user', async () => {
@@ -53,6 +70,24 @@ test('Should get profile for user', async () => {
 test('Should not get profile for unauthorized user', async () => {
     await request(app)
         .get('/users/me')
+        .send()
+        .expect(401)
+})
+
+test('Should delete account for user', async () => {
+    await request(app)
+        .delete('/users/me')
+        .set('Authorization', `Bearer ${user1.tokens[0].token}`)
+        .send()
+        .expect(200)
+
+    const user = await User.findById(_id1)
+    expect(user).toBeNull()
+})
+
+test('Should not delete account for unauthenticate user', async () => {
+    await request(app)
+        .delete('/users/me')
         .send()
         .expect(401)
 })
